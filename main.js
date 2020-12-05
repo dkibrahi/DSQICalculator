bfe66bb12d0f7a366044901c3f59695fad3d04fc
Daniel Kareem Ibrahim <dkibrahi@umich.edu>
2020-12-05 14:43:32 -0500

    Added basic functionality for help function for the s1 value

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
 $(document).ready(function() {
     // ALL CODE GOES IN HERE
+    var sValueRules = [
+      [
+        'S1 represents the total number of modules',
+        'S1 cannot be a negative number or 0',
+        'S1 must be greater than S2, S3, and S7'
+      ]
+    ]; // Store rules that each s value must abide by
+
+    // Functions
 
     function collectSValues() {
         var s1Value = $("#s1").val();
@@ -11,6 +20,20 @@ $(document).ready(function() {
         var s7Value = $("#s7").val();
         return [s1Value, s2Value, s3Value, s4Value, s5Value, s6Value, s7Value];
     }
+
+
+    // Event Functions
+
+    $("#s1Help").click(function() {
+        $("#s1Help").append("<div id = 's1Information'></div>");
+        $("#s1Information").append("<ul id = 's1UL'></ul>");
+        var index = 0;
+        for (index; index < sValueRules[0].length; index++) {
+            $("#s1UL").append("<li> " + sValueRules[0][index] + " </li>"); // Output all the rules for that s value
+        }
+
+    });
+
 
     $("#nextPageButton").click(function() {
         var sValues = collectSValues(); // Get all the s1values from the user
